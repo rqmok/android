c90551c61ed2c8307d7a4f0221f651c8dbaa7073
Zeeshan <rqmok@hotmail.com>
2016-07-18 19:48:04 +0930

    updates: Arrange cherries in descending order.
    Change-Id: I4ce1acca24252e2533fea0ca4367f65ef4427ff1

--- updates.sh
+++ updates.sh
@@ -7,18 +7,6 @@ fi
 
 android="${PWD}"
 
-# Revert "Revert "Reenable support for non-PIE executables""
-cherries+=(117733)
-
-# arm: Allow disabling PIE for dynamically linked executables
-cherries+=(123032)
-
-# policy: Show a simple dialog about optimizing apps
-cherries+=(131627)
-
-# SystemServer: Do not start Atlas Service on lowram devices.
-cherries+=(148998)
-
 # Implement UICC TLV Data decoding.
 cherries+=(114381)
 
@@ -30,6 +18,18 @@ cherries+=(114805)
 
 # UiccController: use registerForAvailable only when persist.radio.apm_sim_not_pwdn is enabled
 cherries+=(114806)
+
+# Revert "Revert "Reenable support for non-PIE executables""
+cherries+=(117733)
+
+# arm: Allow disabling PIE for dynamically linked executables
+cherries+=(123032)
+
+# policy: Show a simple dialog about optimizing apps
+cherries+=(131627)
+
+# SystemServer: Do not start Atlas Service on lowram devices.
+cherries+=(148998)
 
 if [ -z $cherries ]
 then
